d3a6a37ffbab448cdd071b7e84afe0dde68755f8
tohatsuwahyu <tohatsu.wbp@gmail.com>
2025-10-25 11:23:02 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+/* config.js — simpan sejajar dengan index.html */
+window.CONFIG = {
+  BASE_URL: 'https://script.google.com/macros/s/AKfycbwQdJUFnQNglb4PNRFulDaXnmdatFqPI2hw0oSxJgUKrNV0i7BuGzh-mgN2ViuyzdsCPg/exec',
+  API_KEY : 'supersecret123'
+};
